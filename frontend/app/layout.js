8f409f100424c61ff1f48914b4c2f51caadee018
ShahAli <senayousofiali@gmail.com>
2023-05-17 23:29:45 +0500

    navbar hero section

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,7 @@
-import Sidebar from '@/components/Sidebar/Sidebar'
+import Navbar from '@/components/Navbar/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import HeroSection from '@/components/HeroSection/HeroSection'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Sidebar />
+        <div aria-hidden="true" className="flex absolute -top-96 left-1/2 transform -translate-x-1/2">
+          <div className="bg-gradient-to-r from-violet-700 to-purple-50 blur-3xl w-full h-full rotate-[-60deg] transform -translate-x-[10rem] dark:from-violet-900/50 dark:to-purple-900"></div>
+          <div className="bg-gradient-to-tl from-blue-50 via-blue-100 to-blue-50 blur-3xl w-full h-full rounded-fulls origin-top-left -rotate-12 -translate-x-[15rem] dark:from-indigo-900/70 dark:via-indigo-900/70 dark:to-blue-900/70"></div>
+        </div>
+        <Navbar />
+        <HeroSection />
         {children}
       </body>
     </html>
